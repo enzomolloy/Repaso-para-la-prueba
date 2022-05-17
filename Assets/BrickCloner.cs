26e7e5fdcfddec72d7aca3a36112e4c7890ad35e
enzomolloy <enzomolloy@gmail.com>
2022-05-17 15:55:03 -0300

    Repaso

---
+++ Assets/BrickCloner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+    public GameObject brickprefab;
+    public float xOffset;
+    public Text userOffset;
+    int counter = 0;
+
+    void Start()
+    {
+      
+    }
+
+
+    public void CloneBrick()
+    {
+        if(counter == 0)
+        {
+            xOffset = float.Parse(userOffset.text);
+        }
+
+        if (counter <5)
+        { 
+        GameObject clon;
+        clon = Instantiate(brickprefab);
+        clon.transform.position += new Vector3(float.Parse(userOffset.text), 0, 0);
+        xOffset += 1.1f;
+        counter++;
+        }
+    }
+}
